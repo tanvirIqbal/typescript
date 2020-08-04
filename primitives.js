fb11a2fb17346cea0ee6aef62fb284a95b0cddb2
tanviriqbal <tanviriqbal4u@live.com>
2020-08-04 11:58:54 +0600

    primitive types

---
+++ primitives.js
@@ -0,0 +1,41 @@
+// booleans
+var authorized;
+authorized = true;
+authorized = false;
+authorized = null;
+authorized = undefined;
+authorized = (function () { return true; })();
+authorized = (function () { return false; })();
+// number
+var myNumber;
+myNumber = 10;
+myNumber = -10;
+myNumber = 10.10;
+myNumber = -10.10;
+myNumber = 10;
+myNumber = 0x123;
+myNumber = 83;
+myNumber = (function () { return 123; })();
+myNumber = null;
+myNumber = undefined;
+// String
+var userName;
+userName = "tanviriqbal";
+userName = 'tanviriqbal';
+userName = "tanviriqbal";
+userName = "tanviriqbal".toUpperCase();
+userName = null;
+userName = undefined;
+userName = (function () { return 'tanviriqbal'; })();
+// null
+var data;
+data = null;
+data = undefined;
+data = (function () { return null; })();
+data = (function () { return undefined; })();
+// undefined
+var userdata;
+userdata = null;
+userdata = undefined;
+userdata = (function () { return null; })();
+userdata = (function () { return undefined; })();
---
+++ primitives.ts
@@ -0,0 +1,50 @@
+// booleans
+
+let authorized: boolean;
+authorized = true;
+authorized = false;
+authorized = null;
+authorized = undefined;
+authorized = (() => true)();
+authorized = (() => false)();
+
+// number
+
+let myNumber: number;
+myNumber = 10;
+myNumber = -10;
+myNumber = 10.10;
+myNumber = -10.10;
+myNumber = 0b1010;
+myNumber = 0x123;
+myNumber = 0o123;
+myNumber = (() => 123)();
+myNumber = null;
+myNumber = undefined;
+
+// String
+
+let  userName: string;
+userName = "tanviriqbal";
+userName = 'tanviriqbal';
+userName = `tanviriqbal`;
+userName = `tanviriqbal`.toUpperCase();
+userName = null;
+userName = undefined;
+userName = (() => 'tanviriqbal')();
+
+// null
+
+let data: null;
+data = null;
+data = undefined;
+data = (() => null)();
+data = (() => undefined)();
+
+// undefined
+
+let userdata: null;
+userdata = null;
+userdata = undefined;
+userdata = (() => null)();
+userdata = (() => undefined)();
